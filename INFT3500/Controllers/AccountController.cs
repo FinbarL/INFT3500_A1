0b44ddc5751878af30b9c3e0920d1b542480c805
FinbarL <113319479+FinbarL@users.noreply.github.com>
2023-07-24 11:04:44 +1000

    Added user registration

--- INFT3500/Controllers/AccountController.cs
+++ INFT3500/Controllers/AccountController.cs
@@ -1,10 +1,12 @@
 using System.Net.Security;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using INFT3500.Models;
 using INFT3500.ViewModels;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,6 +22,76 @@ public class AccountController : Controller
     public IActionResult Login()
     {
         return View();
+    }
+    
+    public IActionResult Register()
+    {
+        return View();
+    }
+    [Authorize]
+    [HttpGet]
+    public IActionResult UpdateUser()
+    {
+        string username = User.Identity.Name;
+        var user = _context.Users.FirstOrDefault(u => u.UserName == username);
+        var updateUserViewModel = new UpdateUserViewModel
+        {
+            UserName = user.UserName,
+            Email = user.Email,
+            Name = user.Name,
+            IsAdmin = user.IsAdmin ?? false,
+            IsStaff = user.IsStaff ?? false
+        };
+        return View(updateUserViewModel);
+    }
+    [HttpGet]
+    [Route("Account/UpdateUser/{userName}")]
+    public async Task<IActionResult> UpdateUser(string userName)
+    {
+        var user = _context.Users.FirstOrDefault(u => u.UserName == userName);
+        var updateUserViewModel = new UpdateUserViewModel
+        {
+            UserName = user.UserName,
+            Email = user.Email,
+            Name = user.Name,
+            IsAdmin = user.IsAdmin ?? false,
+            IsStaff = user.IsStaff ?? false
+        };
+        if (user != null)
+        {
+            _context.Attach(user);
+            _context.Entry(user).Property(u => u.Email).IsModified = true;
+            _context.Entry(user).Property(u => u.Name).IsModified = true;
+            _context.Entry(user).Property(u => u.IsAdmin).IsModified = true;
+            _context.Entry(user).Property(u => u.IsStaff).IsModified = true;
+            await _context.SaveChangesAsync();
+            return RedirectToAction("UserInfo", "Account");
+        }
+
+        
+        return View(updateUserViewModel);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> UpdateUser(UpdateUserViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == model.UserName);
+            if(user != null)
+            {
+                _context.Attach(user);
+                _context.Entry(user).Property(u => u.Email).IsModified = true;
+                _context.Entry(user).Property(u => u.Name).IsModified = true;
+                _context.Entry(user).Property(u => u.IsAdmin).IsModified = true;
+                _context.Entry(user).Property(u => u.IsStaff).IsModified = true;
+                await _context.SaveChangesAsync();
+                return RedirectToAction("UserInfo", "Account");
+            }
+        }
+        Console.WriteLine("invalid!!!");
+        return View(model);
     }
 
     [HttpPost]
@@ -41,12 +113,75 @@ public class AccountController : Controller
         ModelState.AddModelError("", "ModelState Invalid.");
         return View(model);
     }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var passwordMatches = model.Password == model.ConfirmPassword;
+            var userExists = await _context.Users.AnyAsync(u => u.UserName == model.UserName);
+            var emailExists = await _context.Users.AnyAsync(u => u.Email == model.emailAddress);
+            if (!passwordMatches)
+            {
+                ModelState.AddModelError("ConfirmPassword", "Passwords do not match.");
+                return View(model);
+            }
+            if (userExists)
+            {
+                ModelState.AddModelError("UserName", "Username already exists.");
+                return View(model);
+            }
+            if (emailExists)
+            {
+                ModelState.AddModelError("emailAddress", "Email already in use.");
+                return View(model);
+            }
+            var newUser = new User
+            {
+                UserName = model.UserName,
+                Email = model.emailAddress,
+                Salt = GenerateSalt(),
+                HashPw = HashPassword(model.Password, GenerateSalt()),
+                IsAdmin = false,
+                IsStaff = false
+            };
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+            await Authenticate(newUser);
+            return RedirectToAction("UserInfo", "Account");
+        }
+        ModelState.AddModelError("ConfirmPassword", "ModelState Invalid.");
+        return View(model);
+    }
+    
     [Authorize]
     public IActionResult UserInfo()
     {
         string username = User.Identity.Name;
         var user = _context.Users.FirstOrDefault(u => u.UserName == username);
         return View(user);
+    }
+
+    private string GenerateSalt()
+    {
+        byte[] salt = new byte[128 / 8];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(salt);
+        }
+        return Convert.ToBase64String(salt);
+    }
+    private string HashPassword(string password, string salt)
+    {
+        var bytes = KeyDerivation.Pbkdf2(
+            password: password,
+            salt: Convert.FromBase64String(salt),
+            prf: KeyDerivationPrf.HMACSHA512,
+            iterationCount: 10000,
+            numBytesRequested: 256 / 8);
+
+        return Convert.ToBase64String(bytes);
     }
     private async Task Authenticate(User user)
     {
